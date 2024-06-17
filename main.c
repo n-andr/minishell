0179deaa817a5c70d699d8a4726399f9ffb7ebd9
lotte-detaeye <lieselot.detaeye@gmail.com>
2024-06-17 16:22:10 +0200

    added comments mapping architecture of the program

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nandreev <nandreev@student.42berlin.de     +#+  +:+       +#+        */
+/*   By: lde-taey <lde-taey@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 14:34:55 by nandreev          #+#    #+#             */
-/*   Updated: 2024/06/17 16:03:35 by nandreev         ###   ########.fr       */
+/*   Updated: 2024/06/17 16:20:42 by lde-taey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,29 @@ void	print_message(void)
 
 
 
-int	main()
+int	main(int argc, char **argv)
 {
 	char	user_input[MAX_INPUT_SIZE];
 	pid_t	pid;
 	int	status;
 
+	// 1. handle arguments
+	if (argc != 1 || argv[1])
+		return (error_message()); // TODO
+	//	1a. store environment variables from the parent process in a struct
+	//	1b. add data about current directory and other states to struct
+	
+	// Read-Eval-Print Loop
 	while (1)
 	{
+		//	2. display prompt message
 		write (1, "$ ", 2);
+		//	3. listen for input with a getline function
 		read_input(user_input);
+		// 4. build lexer that scans input and puts everything in an array
+		// 5. parse array and put everything into an execution tree
+		// 6. expand
+		// 7. execute the commands with a fork
 		pid = fork();
 		
 		if (pid < 0) // error
@@ -64,6 +77,8 @@ int	main()
 		{
 			waitpid(pid, &status, 0);
 		}
+		//	8. free memory
+
 	}
 	return (0);
 }
