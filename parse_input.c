b05f4dd0b87fb56650863be102cdfa8566b467b5
Natalia <nandreev@student.42berlin.de>
2024-08-12 15:16:46 +0200

    exit segfault fix

--- parse_input.c
+++ parse_input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_input.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nandreev <nandreev@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: nandreev <nandreev@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 16:00:10 by nandreev          #+#    #+#             */
-/*   Updated: 2024/08/12 00:24:23 by nandreev         ###   ########.fr       */
+/*   Updated: 2024/08/12 15:15:43 by nandreev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
