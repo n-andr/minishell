2518dc6b0c03f9529db2195796d089d8db86b7fd
lotte-detaeye <lieselot.detaeye@gmail.com>
2024-08-15 15:51:09 +0200

    exit codes and redirections

--- cleanup.c
+++ cleanup.c
@@ -67,6 +67,7 @@ void	free_commans(t_minishell *shell)
 		free_commands_args(shell->commands);
 		free_commands_redir(shell->commands);
         free(shell->commands);
+		shell->pid = 0; // check again
         shell->commands = next;
     }
 }
@@ -79,9 +80,5 @@ void	free_everything(t_minishell *shell)
 	free(shell->pwd);
 	free(shell->oldpwd);
 	free_array(shell->paths);
-	//free(shell->commands->args);
-	//free_array(shell->commands->redir); // sth wrong here
-	// free(shell->commands->args);
-	// free_array(shell->commands->redir); // sth wrong here
 	free(shell->commands);
 }
--- pipe.c
+++ pipe.c
@@ -6,7 +6,7 @@
 /*   By: lde-taey <lde-taey@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 16:29:57 by lde-taey          #+#    #+#             */
-/*   Updated: 2024/08/12 16:50:28 by lde-taey         ###   ########.fr       */
+/*   Updated: 2024/08/15 15:47:10 by lde-taey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	child_process(int *pipe_fd, t_minishell *shell, t_args *command, int *in_fd
 		close(pipe_fd[1]);
 	}
 	close(pipe_fd[0]);
+	// check for builtins here? special treatment of cd and exit?
 	handle_cmd(shell, command);
 	exit(0);
 }
@@ -42,9 +43,10 @@ void	parent_process(int *pipe_fd, int *in_fd)
 int	ft_pipe(t_minishell *shell)
 {
 	int		pipe_fd[2];
-	pid_t	child_pid;
+	pid_t	child_pid; // use the value in the struct instead
 	t_args	*temp;
 	int		in_fd;
+	int		status;
 
 	in_fd = -1;
 	temp = shell->commands;
@@ -60,7 +62,7 @@ int	ft_pipe(t_minishell *shell)
 			pipe_fd[0] = -1;
 			pipe_fd[1] = -1;
 		}
-		child_pid = fork();
+		child_pid = fork(); // store in struct
 		if (child_pid == -1)
 		{
 			perror("child process");
@@ -71,7 +73,14 @@ int	ft_pipe(t_minishell *shell)
 		parent_process(pipe_fd, &in_fd);
 		temp = temp->next;
 	}
-	while (wait(NULL) > 0);
+	// while (wait(NULL) > 0);
+	while ((child_pid = waitpid(-1, &status, 0)) > 0)
+	{
+		if (WIFEXITED(status))
+			shell->exit_code = WEXITSTATUS(status);
+		else
+			shell->exit_code = EXIT_FAILURE;
+	}
 	free (temp);
 	return (1);
 }
