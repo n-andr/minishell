5ecfaff62f95197cb9488bb1a2e733490ccda5bf
lotte-detaeye <lieselot.detaeye@gmail.com>
2024-09-11 13:50:14 +0200

    norminette cd

--- builtins/mini_cd.c
+++ builtins/mini_cd.c
@@ -6,7 +6,7 @@
 /*   By: lde-taey <lde-taey@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 18:53:11 by lde-taey          #+#    #+#             */
-/*   Updated: 2024/09/05 15:30:56 by lde-taey         ###   ########.fr       */
+/*   Updated: 2024/09/11 13:48:38 by lde-taey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static void	add_environmentals(t_minishell *shell, char *lastdir)
 {
 	char	newdir[MAX_INPUT_SIZE];
-	
+
 	free(shell->oldpwd);
 	shell->oldpwd = ft_strdup(lastdir);
 	if (getcwd(newdir, sizeof(newdir)))
@@ -38,31 +38,10 @@ static void	check_relative_path(char *arg, t_minishell *shell)
 	}
 }
 
-/* 
-this function
-0. stores the current directory for later
-1. checks if the argument is - or nothing (chdir treats '..', '/' 
-and '~' automatically, so these are the only two special cases)
-2. checks for relative paths and updates the arg with environmentals if needed
-3. checks if the directory change was successful by checking the return value
-4. if yes, it updates the environmental values
-*/
-int	mini_cd(t_minishell *shell, t_args *cmd)
+static int change_directory(t_args *cmd, t_minishell *shell)
 {
 	int		ret;
-	char	lastdir[MAX_INPUT_SIZE];
-
-	if (cmd->args[2])
-	{
-		too_many_args_error();
-		shell->exit_code = 1;
-		return (1);
-	}
-	if (!getcwd(lastdir, sizeof(lastdir)))
-	{
-		shell->exit_code = 1;
-		return (1);
-	}
+	
 	if (!ft_strcmp(cmd->args[1], "-"))
 		ret = chdir(shell->oldpwd);
 	else if (!strcmp(cmd->args[1], ""))
@@ -78,6 +57,34 @@ int	mini_cd(t_minishell *shell, t_args *cmd)
 		shell->exit_code = 1;
 		return (1);
 	}
+	return (0);
+}
+/* 
+this function
+0. stores the current directory for later
+1. checks if the argument is - or nothing (chdir treats '..', '/' 
+and '~' automatically, so these are the only two special cases)
+2. checks for relative paths and updates the arg with environmentals if needed
+3. checks if the directory change was successful by checking the return value
+4. if yes, it updates the environmental values
+*/
+int	mini_cd(t_minishell *shell, t_args *cmd)
+{
+	char	lastdir[MAX_INPUT_SIZE];
+
+	if (cmd->args[2])
+	{
+		too_many_args_error();
+		shell->exit_code = 1;
+		return (1);
+	}
+	if (!getcwd(lastdir, sizeof(lastdir)))
+	{
+		shell->exit_code = 1;
+		return (1);
+	}
+	if (change_directory(cmd, shell) == EXIT_FAILURE)
+		return (1);
 	add_environmentals(shell, lastdir);
 	return (0);
 }
