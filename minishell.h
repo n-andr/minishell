009b63368810dc02765528bc06bfcbe2bbfa3ee3
lotte-detaeye <lieselot.detaeye@gmail.com>
2024-09-11 17:29:08 +0200

    norminette header file

--- minishell.h
+++ minishell.h
@@ -6,74 +6,71 @@
 /*   By: lde-taey <lde-taey@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 14:48:02 by nandreev          #+#    #+#             */
-/*   Updated: 2024/09/11 17:14:18 by lde-taey         ###   ########.fr       */
+/*   Updated: 2024/09/11 17:25:09 by lde-taey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <stdbool.h>
-#include <string.h>
-#include <signal.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <stdbool.h>
+# include <string.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft.h"
 
-#ifndef MAX_INPUT_SIZE
-# define MAX_INPUT_SIZE 1024
+# ifndef MAX_INPUT_SIZE
+#  define MAX_INPUT_SIZE 1024
+# endif
 
-typedef struct s_args t_args;
+typedef struct s_args	t_args;
 
 typedef struct s_storefd
 {
 	int		stdin;
 	int		stdout;
-} t_storefd;
+}	t_storefd;
 
 typedef struct s_args
 {
-	char 	**args;
-	char 	**redir;
+	char	**args;
+	char	**redir;
 	bool	is_redir;
 	bool	cmd_valid; //maybe not needed
 	size_t	is_pipe;
 	t_args	*next;
-	t_args *previous;
+	t_args	*previous;
 	char	*heredoc;
 	int		childpid;
 }	t_args;
 
 typedef struct s_minishell
 {
-	char	**args;
-	char	**envs;
-	char	**paths;
-	char	*pwd;
-	char	*oldpwd;
-	char	*home;
-	int		exit_code;
-	t_args	*commands;
-	int		pid;
-	int		fd_in;
+	char		**args;
+	char		**envs;
+	char		**paths;
+	char		*pwd;
+	char		*oldpwd;
+	char		*home;
+	int			exit_code;
+	t_args		*commands;
+	int			pid;
+	int			fd_in;
 	t_storefd	*fds;
 }	t_minishell;
 
-void	init_environmentals(char **env,t_minishell *shell);
+void	init_environmentals(char **env, t_minishell *shell);
 void	signal_config(void);
 // utils
-/* char	*ft_strdup(const char *s);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlen(const char *s); */
-int		ft_strcmp(const char *s1, const char *s2); /*
-int		ft_strncmp(const char *s1, const char *s2, size_t n); */
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcat(char *dest, char *src);
 char	*ft_strcpy(char *dest, char *src);
-/*void	ft_bzero(void *s, size_t n);
-int		ft_isdigit(int c); */
 // errors
 void	args_error(void);
 void	malloc_error(t_minishell *shell);
@@ -91,8 +88,8 @@ void	expand_command(t_minishell *shell, t_args *command);
 // check
 bool	check_if_cmd_valid(t_minishell *shell, t_args *command);
 int		is_builtin(char *str);
-int 	is_executable(t_minishell *shell, char *str);
-int 	is_path(char *str);
+int		is_executable(t_minishell *shell, char *str);
+int		is_path(char *str);
 // execute
 void	execute(t_minishell *shell);
 void	handle_cmd(t_minishell *shell, t_args *command);
@@ -107,7 +104,6 @@ int		execbuiltin(t_minishell *shell, t_args *cmd);
 void	child_signals(void);
 void	sigquit_handler(int sig);
 void	single_cmd(t_minishell *shell, t_args *cmd);
-
 // builtins
 int		mini_pwd(t_minishell *shell);
 int		mini_cd(t_minishell *shell, t_args *cmd);
@@ -117,7 +113,6 @@ int		mini_echo(t_args *cmd);
 int		mini_export(t_minishell *shell, t_args *cmd);
 void	mini_exit(t_minishell *shell);
 int		mini_export(t_minishell *shell, t_args *cmd);
-
 // cleanup
 void	free_everything(t_minishell *shell);
 void	free_array(char **array);
@@ -126,10 +121,8 @@ void	directory_check(t_minishell *shell, char *cmd);
 void	command_check(t_minishell *shell, char *cmd);
 // free
 void	free_args(t_minishell *shell);
-
 // tests
 void	test_printf(t_minishell *shell); //delete
 void	test_printf_command(t_args	*temp); //delete
-
 
 #endif
