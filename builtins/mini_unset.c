45605c8e101b9772af22f4cec4b694e20e01b6de
lotte-detaeye <lieselot.detaeye@gmail.com>
2024-09-11 14:10:42 +0200

    norminette pwd and unset

--- builtins/mini_unset.c
+++ builtins/mini_unset.c
@@ -6,7 +6,7 @@
 /*   By: lde-taey <lde-taey@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 12:20:49 by lde-taey          #+#    #+#             */
-/*   Updated: 2024/09/10 17:22:54 by lde-taey         ###   ########.fr       */
+/*   Updated: 2024/09/11 14:08:29 by lde-taey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,5 @@ int	mini_unset(t_minishell *shell, t_args *cmd)
 	if (check_if_valid(cmd) == 1)
 		return (EXIT_SUCCESS);
 	delete_var(shell->envs, cmd->args[1]);
-	// mini_env(shell); // test
 	return (EXIT_SUCCESS);
 }
