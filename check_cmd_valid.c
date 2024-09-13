f40d39d4739f9c3e66025fc928ebbed54b518242
Natalia <nandreev@student.42berlin.de>
2024-09-13 15:03:10 +0200

    norm WIP

--- check_cmd_valid.c
+++ check_cmd_valid.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_cmd_valid.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nandreev <nandreev@student.42berlin.de     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/13 15:02:54 by nandreev          #+#    #+#             */
+/*   Updated: 2024/09/13 15:02:56 by nandreev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 int	is_builtin(char *str)
@@ -56,40 +68,39 @@ int 	is_executable(t_minishell *shell, char *str)
     return (0);
 }
 
-int 	is_path(char *str)
+int	is_path(char *str)
 {
-	if(str == NULL)
+	if (str == NULL)
 		return (0);
 	if (ft_strchr(str, '/'))
 		return (1);
 	else
-		return(0);
+		return (0);
 }
 
-bool check_if_cmd_valid(t_minishell *shell, t_args *command)
+bool	check_if_cmd_valid(t_minishell *shell, t_args *cmd)
 {
-	if (command == NULL)
+	if (cmd == NULL)
 		return (true);
-	if ((command->args == NULL || command->args[0] == NULL )
-		&& command->is_redir == 0 && command->is_pipe == 0)
+	if ((cmd->args == NULL || cmd->args[0] == NULL )
+		&& cmd->is_redir == 0 && cmd->is_pipe == 0)
 	{
-		//to be checked on school computer
 		return (true);
 	}
-	else if (command->args == NULL && command->is_redir == 0 && command->is_pipe != 0)
+	else if (cmd->args == NULL && cmd->is_redir == 0 && cmd->is_pipe != 0)
 	{
-		write(2, "minishell: syntax error near unexpected token `|'\n", 50); //to be checked on school computer
+		write(2, "minishell: syntax error near unexpected token `|'\n", 50);
 		return (false);
 	}
-	else if (command->args != NULL && (is_builtin(command->args[0]) 
-		|| is_executable(shell, command->args[0])  
-		|| is_path(command->args[0])))
+	else if (cmd->args != NULL && (is_builtin(cmd->args[0]) 
+			|| is_executable(shell, cmd->args[0])
+			|| is_path(cmd->args[0])))
 		return (true);
-	else if (command->is_redir == 1 && command->args == NULL)
+	else if (cmd->is_redir == 1 && cmd->args == NULL)
 		return (true);
 	else
 	{
-		write(2, command->args[0], ft_strlen(command->args[0]));
+		write(2, cmd->args[0], ft_strlen(cmd->args[0]));
 		write(2, ": command not found\n", 20);
 		return (false);
 	}
