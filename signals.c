7f262c190876df4c3b750114e143a0770475d87c
lotte-detaeye <lieselot.detaeye@gmail.com>
2024-09-13 15:22:00 +0200

    signals working for child processes

--- signals.c
+++ signals.c
@@ -6,16 +6,28 @@
 /*   By: lde-taey <lde-taey@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 14:46:29 by lde-taey          #+#    #+#             */
-/*   Updated: 2024/09/13 15:01:19 by lde-taey         ###   ########.fr       */
+/*   Updated: 2024/09/13 15:10:17 by lde-taey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	child_signals(void)
+/* void	child_signals(void)
 {
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
+} */
+
+void	signal_config_execute(void)
+{
+	struct sigaction	signal_act;
+
+	signal_act.sa_handler = sigint_handler_exec;
+	sigemptyset(&signal_act.sa_mask);
+	signal_act.sa_flags = 0;
+	sigaction(SIGINT, &signal_act, NULL);
+	signal_act.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &signal_act, NULL);
 }
 
 void	signal_config_input(void)
